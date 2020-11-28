5dc5a44089e5f9c0c4bcee1dad0b8fbcf4efaed4
snehasishm20 <75127325+snehasishm20@users.noreply.github.com>
2020-11-28 20:47:56 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int x,y,sum,sub,multi;
+printf("please enter two numbers\n");
+scanf("%d%d",&x,&y);
+sum=x+y;
+sub=x-y;
+multi=x*y;
+printf("sum of two numbers=%d\n",sum);
+printf("subtraction of two numbers=%d\n",sub);
+printf("multiplication of two number=%d\n",multi);
+return 0;
+}
+
