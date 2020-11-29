8fa843f5657bbb1de888747aad6a17dd9a0c1800
snehasishm20 <75127325+snehasishm20@users.noreply.github.com>
2020-11-29 22:35:28 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+float main()
+{
+float ctemp,ftemp;
+printf("enter temprature in celcius");
+scanf("%f",&ctemp);
+ftemp=(ctemp*9/5)+32;
+printf("temprature in faraneit=%f",ftemp);
+return 0;
+}
