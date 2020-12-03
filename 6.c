79e970463a3dbf564b77c45fe3a9a1d3e3c31d59
snehasishm20 <75127325+snehasishm20@users.noreply.github.com>
2020-12-03 23:02:35 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
 {
-printf("Name: Mr. Vedant Acharya\nRoll No.:16BTCSE38\nProgramme Name: B.Tech. CSE\nsemester: I\nplace: Jyoti Vihar, Burla\n");
+printf("Name: Mr. Snehasish Mohapatra\nRoll No.:16BTCSE28\nProgramme Name: B.Tech. CSE\nsemester: I\nplace: Jyoti Vihar, Burla\n");
 return 0;
 }
