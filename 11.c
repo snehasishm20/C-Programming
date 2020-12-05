41fe6692ab30adc572fd82e2e31583ac1dd09fef
snehasishm20 <75127325+snehasishm20@users.noreply.github.com>
2020-12-05 22:20:51 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+float N1,N2,N3,N4;
+printf(" Enter the work done by N1=");
+scanf("%f",&N1);
+printf(" Enter the work done by N2=");
+scanf("%f",&N2);
+printf(" Enter the work done by N3=");
+scanf("%f",&N3);
+N4=(N1+N2+N3)/3;
+printf("the total work done is N4=%f",N4);
+return 0;
+}
+
